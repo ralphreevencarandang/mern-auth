a6d8c590408ec4ce3ec539c792a45d7e365d04c4
Ralph Carandang <rvncrndng@gmail.com>
2025-06-30 20:10:10 +0800

    Modify the cors backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5001
 await connectDB();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(cookieParser())
 
 app.use('/api', authRoute)
